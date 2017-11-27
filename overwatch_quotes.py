9945cdec8945f9e193f7bb6ef794d6280584fb76
Joshua Rudio <joshuarudio@gmail.com>
2017-11-27 14:34:32 -0800

    Comment out current code

--- overwatch_quotes.py
+++ overwatch_quotes.py
@@ -8,19 +8,19 @@ reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit('overwatch')
 
 # Reads Top 5 stories in 'Hot' category of /r/Overwatch
-for submission in subreddit.hot(limit=5):
-    print("Title: ", submission.title)
-    print("Text: ", submission.selftext)
-    print("Score: ", submission.score)
-    print("---------------------------------\n")
-
-if not os.path.isfile("posts_replied_to.txt"):
-    posts_replied_to =[]
-else:
-    with open("posts_replied_to.txt", "r") as f:
-        posts_replied_to = f.read()
-        posts_replied_to = posts_replied_to.split("\n")
-        posts_replied_to = list(filter(None, posts_replied_to))
+# for submission in subreddit.hot(limit=5):
+#     print("Title: ", submission.title)
+#     print("Text: ", submission.selftext)
+#     print("Score: ", submission.score)
+#     print("---------------------------------\n")
+#
+# if not os.path.isfile("posts_replied_to.txt"):
+#     posts_replied_to =[]
+# else:
+#     with open("posts_replied_to.txt", "r") as f:
+#         posts_replied_to = f.read()
+#         posts_replied_to = posts_replied_to.split("\n")
+#         posts_replied_to = list(filter(None, posts_replied_to))
 
 DOOMFIST_QUOTES = (
     'Rising above all!',
